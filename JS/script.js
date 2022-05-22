abc655a7bc71a83b09f4868a91cebc62ccff7b34
Szymon Forst <szymonforstbiznes@gmail.com>
2022-05-22 20:43:03 +0200

    added ability to add new tasks

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+{
+  const hello = () => {
+    console.log("Witam :)")
+  }
+
+  const tasks = [
+{
+  content: "ubrać się",
+  done: false },
+
+{
+  content: "umyć się",
+  done: true}, 
+  ];
+
+
+  const render = () => {
+    let htmlString = "";
+
+    for(const task of tasks) {
+      htmlString += `
+      <fieldset class="form__fieldset form__fieldset--list">
+      <ul class="form__list">
+        <li class="form__list--table">
+          <button class="button__done">✔</button>
+          <span class="form__list--content">${task.content}</span>
+          <button class="button__delete">🗑️</button>
+        </li>
+      </ul>
+      </fieldset>
+      `
+    };
+
+  document.querySelector(".js-list").innerHTML = htmlString;
+  }
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+      done: false,
+    });
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-input").value.trim();
+    
+    if(newTaskContent === ""){
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  }
+
+const init = () => {
+  hello();
+  render();
+
+  const formElement = document.querySelector(".js-form");
+  formElement.addEventListener("submit", onFormSubmit);
+};
+
+init();
+
+}
